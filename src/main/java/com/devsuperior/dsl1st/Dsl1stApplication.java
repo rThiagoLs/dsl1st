64424cbd960d06ecd0ded38619e9f30a8b26715e
rThiagoLs <ruanthiago.in@gmail.com>
2025-05-20 19:27:10 -0300

    project created

---
+++ src/main/java/com/devsuperior/dsl1st/Dsl1stApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsl1st;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsl1stApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsl1stApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/dsl1st/Dsl1stApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsl1st;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsl1stApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
